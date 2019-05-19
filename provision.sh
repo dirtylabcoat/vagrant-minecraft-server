b97c42353a8ae57fb68313be615284154481efdb
Daniel Löfquist <daniel@lofquist.org>
2019-05-19 12:51:27 +0200

    Upgrade to 1.14.1

--- provision.sh
+++ provision.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+MINECRAFT_USER=minecraft
+MINECRAFT_GROUP=minecraft
 MINECRAFT_HOME=/opt/minecraft
-DOWNLOAD_URL="https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar"
+## 1.13.2
+#DOWNLOAD_URL="https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar"
+## 1.14.1
+DOWNLOAD_URL="https://launcher.mojang.com/v1/objects/ed76d597a44c5266be2a7fcd77a8270f1f0bc118/server.jar"
 apt-get update
 apt-get upgrade
 apt-get install -y openjdk-8-jre
-adduser --system --home $MINECRAFT_HOME minecraft
-addgroup --system minecraft
-adduser minecraft minecraft
+adduser --system --home $MINECRAFT_HOME $MINECRAFT_USER
+addgroup --system $MINECRAFT_GROUP
+adduser $MINECRAFT_USER $MINECRAFT_GROUP
 curl -s $DOWNLOAD_URL --output $MINECRAFT_HOME/server.jar > /dev/null
 cd $MINECRAFT_HOME
 java -Xmx1024M -Xms1024M -jar server.jar nogui
 sed -i -e 's/false/true/g' eula.txt
-chown -R minecraft:minecraft $MINECRAFT_HOME
+chown -R $MINECRAFT_USER:$MINECRAFT_GROUP $MINECRAFT_HOME
 cp /vagrant/minecraft-server.service /etc/systemd/system/
 chown root:root /etc/systemd/system/minecraft-server.service
 chmod 755 /etc/systemd/system/minecraft-server.service
@@ -19,3 +24,4 @@ systemctl daemon-reload
 systemctl enable minecraft-server.service
 systemctl start minecraft-server
 systemctl status minecraft-server
+
